5591b0c424a52d3f769cad0198f6d1e7b360ea3c
mayank6940 <mayankraj287@gmail.com>
2024-06-25 01:47:45 +0530

    final commit

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-04jjvc1f55xd_dqhwz++p88-p9lh#%&y^g+^5w96u2p#7@m756
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.0.127"]
+ALLOWED_HOSTS = [""]
 
 
 # Application definition

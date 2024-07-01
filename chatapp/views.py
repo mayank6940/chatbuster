ceb4c3bc9e854bd2aef7f80345fd12040734a98e
Mayank Raj <44741414+mayank6940@users.noreply.github.com>
2024-07-02 03:09:05 +0530

    views.py

--- chatapp/views.py
+++ chatapp/views.py
@@ -10,18 +10,18 @@ import json
 import os
 from django.conf import settings
 
-# Path to your CSV file containing names
+
 NAMES_CSV_PATH = os.path.join(settings.BASE_DIR, 'static\media\LastNames.csv')
 
-# Load names from CSV into a list
+
 with open(NAMES_CSV_PATH, newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     names_list = list(reader)
 
-# Function to clear the database every 12 hours
+
 def clear_database():
     while True:
-        time.sleep(43200)  # 12 hours in seconds
+        time.sleep(43200)  
         ref = db.reference('/')
         ref.set({})
 
@@ -29,13 +29,13 @@ threading.Thread(target=clear_database, daemon=True).start()
 
 def join_group(request):
     if request.method == 'POST':
-        # Generate a random name from the loaded names list
-        username = random.choice(names_list)[0]  # Assuming the CSV has names in the first column
+       
+        username = random.choice(names_list)[0]  
 
-        # Generate a random character (or use a predefined set)
+        
         character = get_random_string(1)
 
-        # Store username and character in session
+        
         request.session['username'] = username
         request.session['character'] = character
 
@@ -51,7 +51,7 @@ def chat(request):
     with open(firebase_config_path) as f:
         service_account_key = json.load(f)
 
-    # Construct the Firebase config object for the front-end
+   
     firebase_config = {
         "apiKey": "332590759bfdff895137d555195cc1a23d8128ba",
         "authDomain": f"{service_account_key['project_id']}.firebaseapp.com",
@@ -65,7 +65,7 @@ def chat(request):
     return render(request, 'chat.html', {
         'username': request.session['username'],
         'character': request.session['character'],
-        'firebase_config': json.dumps(firebase_config)  # Pass as JSON string
+        'firebase_config': json.dumps(firebase_config) 
     })
 
 def logout_view(request):
